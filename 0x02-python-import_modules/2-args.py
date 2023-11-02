7b4c31bf7bb7b0a321222219422b2e4d3a29d52e
root <eslamelmishtawy@gmail.com>
2023-11-02 04:05:29 -0700

    s

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+arguments = len(sys.argv) - 1
+if arguments == 0:
+    print("0 arguments.")
+else:
+    if arguments == 1:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
+    else:
+        print("{} arguments:".format(arguments))
+        for i in range(1, arguments+1):
+            print("{}: {}".format(i, sys.argv[i]))
+
+
