37c26ab81707ae0be729a368ac71c319306821ed
root <eslamelmishtawy@gmail.com>
2023-11-10 04:51:05 -0800

    s

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
     best_score = next(iter(a_dictionary.values()))
+    name = next(iter(a_dictionary.keys()))
     for i, j in a_dictionary.items():
         if j > best_score:
             best_score = j
-    return best_score
+            name = i
+    return name
--- 0x04-python-more_data_structures/main.py
+++ 0x04-python-more_data_structures/main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-uniq_add = __import__('2-uniq_add').uniq_add
+best_score = __import__('10-best_score').best_score
 
-my_list = [1, 2, 3, 1, 4, 2, 5]
-result = uniq_add(my_list)
-print("Result: {:d}".format(result))
+a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
+best_key = best_score(a_dictionary)
+print("Best score: {}".format(best_key))
+
+best_key = best_score(None)
+print("Best score: {}".format(best_key))
