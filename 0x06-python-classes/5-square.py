16a78eaae073957fe2dc800ead9bc1694632fd6b
root <eslamelmishtawy@gmail.com>
2023-11-21 14:00:28 -0800

    ss

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -40,6 +40,6 @@ class Square:
         if self.__size <= 0:
             print()
         for i in range(self.__size):
-            for j in range(1, self.__size):
+            for j in range(self.__size):
                 print("#", end='')
             print()
