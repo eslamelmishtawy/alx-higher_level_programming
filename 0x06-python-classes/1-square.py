1c2531f27da496bb492af8fdb8014ccf347e221b
root <eslamelmishtawy@gmail.com>
2023-11-21 13:46:39 -0800

    ss

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Square Class"""
+
+
+class Square:
+    """Square Class"""
+    def __init__(self, size):
+        """Initialize square
+
+        Args:
+        size(int): size of square
+        """
+        self.__size = size
