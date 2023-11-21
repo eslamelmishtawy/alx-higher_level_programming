600cf826aa3b5e3d390ff2de60753ce0fc07a4da
root <eslamelmishtawy@gmail.com>
2023-11-21 13:51:28 -0800

    ss

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""Square Class"""
+
+
+class Square:
+    """Square Class"""
+    def __init__(self, size=0):
+        """Initialize square
+
+        Args:
+        size(int): size of square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    
+    def area(self):
+        """get square area"""
+        return (self.__size * self.__size)
