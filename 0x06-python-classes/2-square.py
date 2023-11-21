372aa06a0b906bd750759ea745355c3be48c9b9a
root <eslamelmishtawy@gmail.com>
2023-11-21 13:48:45 -0800

    ss

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Square Class"""
+
+
+class Square:
+    """Square Class"""
+    def __init__(self, size=0):
+        """Initialize square
+
+        Args:
+        size(int): size of square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
