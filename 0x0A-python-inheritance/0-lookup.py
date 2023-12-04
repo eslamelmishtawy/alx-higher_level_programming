62c242b967a471d1a8d934bf067243d0ff9367bc
root <eslamelmishtawy@gmail.com>
2023-12-04 14:17:17 -0800

    s

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""lookup module"""
+
+
+def lookup(obj):
+    """
+    look up function
+    Args:
+        obj: An object
+    Return: attributes and method of object
+    """
+    return dir(obj)
