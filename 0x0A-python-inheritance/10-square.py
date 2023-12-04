f691ba8ebb579ed1b2fd91f95fa59a853c0dd98e
root <eslamelmishtawy@gmail.com>
2023-12-04 15:33:30 -0800

    s

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Square Module
+"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square class
+    """
+    def __init__(self, size):
+        """
+        init square
+        Args:
+            size
+        """
+        self.integer_validator("size", size)
+        self.__size = size
