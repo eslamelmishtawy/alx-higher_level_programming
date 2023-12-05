9da0e560d1a91db86bce626db9162aa5853e0afd
root <eslamelmishtawy@gmail.com>
2023-12-05 04:35:25 -0800

    ss

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+file module
+"""
+import json
+
+def to_json_string(my_obj):
+    """
+    covert object to json
+    Args:
+        obj
+    """
+    return json.dumps(my_obj)
