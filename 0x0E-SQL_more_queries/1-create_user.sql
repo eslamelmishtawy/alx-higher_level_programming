42b571d815dfe3366ccbcd37128d37e9348edb07
root <eslamelmishtawy@gmail.com>
2024-01-17 13:49:32 -0800

    s

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
---- grant all user permission
+-- grant all user permission
 CREATE USER IF NOT EXISTS `user_0d_1`@`localhost` IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO `user_0d_1`@`localhost`;
