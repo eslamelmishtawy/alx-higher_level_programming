d85a006cffdca5f5d3ce666eed82f7c65fe16f4b
root <eslamelmishtawy@gmail.com>
2024-01-17 14:06:48 -0800

    s

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,3 @@
+-- create database and table
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (`id` INT AUTO_INCREMENT UNIQUE NOT NULL, `name` VARCHAR(256) NOT NULL, PRIMARY KEY (`id`));
