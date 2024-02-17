ef29181cc4534a4db9fb632ef08cb4fb6bfcd464
root <eslamelmishtawy@gmail.com>
2024-02-17 14:12:03 -0800

    s

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+list all states
+"""
+
+
+import sys
+import MySQLdb
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         host='localhost',
+                         port=3306)
+
+    cursor = db.cursor()
+
+    sql = """ SELECT * FROM states
+          WHERE name = %s
+          ORDER BY id ASC """
+
+    cursor.execute(sql)
+
+    data = cursor.fetchall()
+
+    for row in data:
+        print(row)
+
+    cursor.close()
+    db.close()
