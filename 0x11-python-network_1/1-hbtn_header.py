7b156f48b3881dbd3e971b3ef3035f0db5d3d916
root <eslamelmishtawy@gmail.com>
2024-03-01 15:54:46 -0800

    s

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+get url from cmd
+"""
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        html = response.info()
+        value = html.get('X-Request-Id')
+        print(value)
