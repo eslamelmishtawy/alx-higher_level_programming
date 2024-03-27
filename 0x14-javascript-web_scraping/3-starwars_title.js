5414ae21b891df3705dba33c8cf9d7794ccb73b5
eslamelmishtawy <eslamelmishtawy@gmail.com>
2024-03-26 17:51:49 -0700

    s

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const request = require('request');
+const api = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request({ url: api, methods: 'GET' }, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(body && JSON.parse(body).title);
+  }
+});
