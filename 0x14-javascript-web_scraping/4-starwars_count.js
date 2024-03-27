e5d07c84f53f19b088808ff34bd6bba8f923a506
eslamelmishtawy <eslamelmishtawy@gmail.com>
2024-03-26 18:00:03 -0700

    s

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,20 +1,19 @@
 #!/usr/bin/node
 
-
 const args = process.argv;
-let url = args[2];
-let request = require('request');
+const url = args[2];
+const request = require('request');
 request(url, function (error, response, body) {
   if (error) {
-    console.log('error:', error); // Print the error if one occurred
+    console.log('error:', error);
   } else {
-    let parsedBody = JSON.parse(body);
-    let results = parsedBody['results'];
+    const parsedBody = JSON.parse(body);
+    const results = parsedBody.results;
     let count = 0;
     for (let i = 0; i < results.length; i++) {
-      let chars = (results[i]['characters']);
+      const chars = (results[i].characters);
       for (let j = 0; j < chars.length; j++) {
-        let check18 = chars[j].endsWith('18/');
+        const check18 = chars[j].endsWith('18/');
         if (check18) {
           count++;
         }
